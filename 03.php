6574a07bcddcfbd08fb93730f06d1b734218c86b
QingZ <shiyanc@139.com>
2017-05-22 09:24:55 +0800

    match phone num

---
+++ 03.php
@@ -0,0 +1,15 @@
+<?php 
+/****
+布尔教育 高端PHP培训
+培  训: http://www.itbool.com
+论  坛: http://www.zixue.it
+****/
+
+$arr = array('13800138000','13426060134','170235','18289881234568782');
+$patt = '/^[0123456789]{11}$/';
+foreach($arr as $k=>$v) {
+	preg_match_all($patt,$v,$src);
+	var_dump($src);
+}
+
+?>
